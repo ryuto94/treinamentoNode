3d53268fa986e36bd2d5a4d91e7c4340d47e54b0
ryuto94 <ryutoguti@hotmail.com>
2019-08-13 22:28:01 -0300

    Add React

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="">
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+                Treinamento Node
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
